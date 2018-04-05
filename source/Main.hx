409c0cbd0a24f66038b8057220ffe237da66bc6a
Seraj Nagi <Lucas_Naruto_AE@Yahoo.com>
2018-04-05 08:53:44 +0200

    Enabled Fullscreen by pressing F Enabled AntiAliasing by pressing A

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,34 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxGame;
 import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.ui.Keyboard;
+import flash.Lib;
 
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
+
+		#if (!flash && !FLX_NO_DEBUG)
+		stage.addEventListener(KeyboardEvent.KEY_DOWN, stage_onKeyDown);
+		#end
+
 		addChild(new FlxGame(1920, 1080, MenuState, 1, 60, 60, true, true));
+
+		Reg.setupRegistry();
+	}
+
+	private function stage_onKeyDown(event:KeyboardEvent):Void
+	{
+		switch (event.keyCode)
+		{
+			case Keyboard.F: FlxG.fullscreen = !FlxG.fullscreen;	
+			case Keyboard.A: FlxG.camera.antialiasing = !FlxG.camera.antialiasing;	
+		}
 	}
 }
